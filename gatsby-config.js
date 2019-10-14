198eb9f9522fc7f3110c07e884beb1b1cced3f45
Lily Brenner <lilyneedsanemail@gmail.com>
2019-10-13 22:34:45 -0400

    Add linting/prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,17 +19,14 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        shortName: `starter`,
+        startUrl: `/`,
+        backgroundColor: `#663399`,
+        themeColor: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-typescript'
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-     `gatsby-plugin-offline`,
+    "gatsby-plugin-typescript"`gatsby-plugin-offline`, // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
   ],
 }
